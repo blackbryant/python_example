8a8111ba569decf0401b83213ee422872c9c851b
blackbryant <e49134030e@gmail.com>
2023-06-23 14:46:20 +0800

    新增pytest教學

---
+++ J.測試框架/sample/calculator.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    return x+y
+
+def minus(x,y):
+    return x-y 
+
+def multiple(x,y):
+    return x*y 
+
+def devide(x,y):
+    return x/y 
+
---
+++ J.測試框架/sample/test_cal.py
@@ -0,0 +1,23 @@
+from calculator import *
+
+def test_add():
+    result_value = add(1,2)
+    except_value=3 
+    assert result_value == except_value
+
+
+def test_minus():
+    result_value = minus(1,2)
+    except_value=-1
+    assert result_value == except_value
+
+def test_devide():
+    result_value = devide(10,2)
+    except_value=5
+    assert result_value == except_value
+
+
+def test_multiple():
+    result_value = multiple(10,2)
+    except_value=20
+    assert result_value == except_value
