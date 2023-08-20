2d8f88e0eb21f76a3284ede97a7a731c2624005d
blackbryant <e49134030e@gmail.com>
2023-08-20 09:36:08 +0800

    add new models.py file and new post class

--- F.網頁框架/Django/myblog/myblog/settings.py
+++ F.網頁框架/Django/myblog/myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dzh+qo=_7rz+zrp#a(^t$m)6j$a5_mav625&p93@&2*y5etdz3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myblog'
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hant'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
